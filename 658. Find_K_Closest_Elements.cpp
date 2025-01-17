7dafe90a1f55ba8130a96d5cf44615c944b449c6
Raj Soren <68795729+therajsoren@users.noreply.github.com>
2025-01-17 06:51:32 +0000

    some fix

--- 658. Find_K_Closest_Elements.cpp
+++ 658. Find_K_Closest_Elements.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 // Given a sorted array, two integers  k and  x, find the  k closest elements to  x in the array. The result should also be sorted in ascending order. If there is a tie, the smaller elements are always preferred.
 
-
 //  Binary search
 class Solution {
 public:
