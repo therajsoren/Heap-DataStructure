6bb375c61cf3e4b219c1a9e4e9dce181ff88590e
Raj Soren <68795729+therajsoren@users.noreply.github.com>
2025-01-17 06:50:35 +0000

    added two pointers approach code

--- 658. Find_K_Closest_Elements.cpp
+++ 658. Find_K_Closest_Elements.cpp
@@ -26,3 +26,19 @@ public:
         return ans;
     }
 };
+
+//  Two pointers
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+       int l = 0, r = arr.size() - 1;
+       while(r - l + 1 > k){
+        if(arr[r] - x < x - arr[l]){
+            l++;
+        } else{
+            r--;
+        }
+       }
+       return vector<int>(arr.begin() + l,arr.begin() + r + 1);
+    }
+};
